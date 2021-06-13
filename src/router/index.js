32f76e8359fd5d8bca2749a02e6329276a24bba6
ygy <tj_ygy@163.com>
2021-06-13 13:59:15 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -63,7 +63,7 @@ export default new Router({
         },{
           path: "/professionals",
           name: "professionals",
-          component: professionals
+          component: professionals  
         },
 
 
